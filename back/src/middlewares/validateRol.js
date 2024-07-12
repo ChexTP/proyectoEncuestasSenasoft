75b5f1ebec17e5023edcb6e9eef6fbcdc9088e01
ChexTP <chechexteroth@gmail.com>
2024-07-11 22:04:08 -0500

    validate rol and token created

---
+++ back/src/middlewares/validateRol.js
@@ -0,0 +1,14 @@
+export const rolRequired = (role) => {
+    return (req, res, next) => {
+        const { user } = req;
+        if (!user) {
+            return res.status(401).json({ message: "No user found" });
+        }
+
+        if (user.role !== role) {
+            return res.status(403).json({ message: "Forbidden: You don't have the right role" });
+        }
+
+        next();
+    };
+};
---
+++ back/src/middlewares/validateToken.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const TOKEN_SECRET = process.env.TOKEN_SECRET
+
+export const authRequired = (req,res,next)=>{
+
+    const {token} = req.cookies
+
+    if(!token) return res.status(401).json({message:"No token, authorization denied"})
+
+        jwt.verify(token,TOKEN_SECRET,(err,user)=>{
+            if(err) return res.status(401).json({message: "Invalid token"});
+ 
+            req.user=user
+
+            next()
+        })
+
+}
